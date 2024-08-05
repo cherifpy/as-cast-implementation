5fd0b959e58913c153736d6ff7b0af9e26d12e27
cherifpy <simocherif2001@gmail.com>
2024-08-05 10:22:23 +0200

    test memcached

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from pymemcache.client import base
+"""from pymemcache.client import base
 import sys
+
 print("hello")
 params = sys.argv[1]
 
@@ -17,4 +18,44 @@ value = client.get('my_key')
 f.write(f'La valeur pour "my_key" est : {value.decode()}')
 
 # Fermer la connexion
-client.close()
+client.close()"""
+
+import pickle
+from pymemcache.client.base import Client
+from pymemcache.serde import Serde
+import sys
+
+
+class PickleSerde(Serde):
+    def serialize(self, key, value):
+        return pickle.dumps(value), 0
+
+    def deserialize(self, key, value, flags):
+        return pickle.loads(value)
+
+
+params = sys.argv[1]
+# Initialize the client with Pickle serialization
+client = Client((params, 11211), serde=PickleSerde())
+
+# Define a sample class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f'Person(name={self.name}, age={self.age})'
+
+# Create an instance of Person
+person = Person('Alice', 30)
+
+# Serialize and set the object in Memcached
+client.set('person_key', person)
+
+# Retrieve and deserialize the object from Memcached
+retrieved_person = client.get('person_key')
+
+f = open("/tmp/log.txt", 'w')
+f.write(retrieved_person)
+
